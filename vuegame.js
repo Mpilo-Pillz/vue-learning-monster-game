20cbf5a2dad04b166a923f2ff29681708e083480
Mpilo Pillz <mpilod@osiristrading.com>
2019-06-14 18:08:49 +0200

    added functionality to special attack, give up  and heal, plus a refactor

--- vuegame.js
+++ vuegame.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
@@ -15,10 +16,14 @@ new Vue({
             // var maxDamage = 10;
             // var minDamage = 3;
 
-            // var damage = this.calculateDamage(3, 10);
+           
             // this.monsterHealth -= damage;
-
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage + ' health points'
+            });
 
             if(this.checkWin()) {
                 return;
@@ -38,18 +43,38 @@ new Vue({
             // damage = this.calculateDamage(2, 11);
             // this.playerHealth -= damage;
 
-            this.playerHealth -= this.calculateDamage(2, 11); 
+            // this.playerHealth -= this.calculateDamage(2, 11); 
+            this.monsterAttacks();
 
             this.checkWin();
         },
         specialAttack: function() {
-
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+            
         },
         heal: function() {
-
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
         giveUp: function() {
-
+            this.gameIsRunning = false;
+        },
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(2, 11);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage + ' health points'
+            });
         },
         calculateDamage: function(minDamage, maxDamage) {
            return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
